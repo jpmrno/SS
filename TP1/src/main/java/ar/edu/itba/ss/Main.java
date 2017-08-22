6d1832d07b603aec75756410f0ee682bc29560f6
Juan <jpmrno@gmail.com>
2017-08-22 09:14:44 -0300

    revert main

--- TP1/src/main/java/ar/edu/itba/ss/Main.java
+++ TP1/src/main/java/ar/edu/itba/ss/Main.java
@@ -6,32 +6,29 @@ import ar.edu.itba.ss.model.Neighbour;
 import ar.edu.itba.ss.model.Particle;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 import java.util.Set;
 import javafx.geometry.Point2D;
 
 public class Main {
 
   public static void main(String[] args) {
-    CellIndexMethod cellIndexMethod = new CellIndexMethod(5, 50, true);
+    final CellIndexMethod cellIndexMethod = new CellIndexMethod(5, 10, true);
     final Map<Particle, Point2D> positions = new HashMap<>();
-    Random r = new Random();
-    for (int i = 0; i < 25; i++) {
-      for (int j = 0; j < 25; j++) {
-        positions.put(new Particle(0.2), new Point2D(i * 2, j * 2));
+    for (double i = 0.5; i < 10; i++) {
+      for (double j = 0.5; j < 10; j++) {
+        positions.put(new Particle(0.4), new Point2D(i, j));
       }
     }
 
-    Map<Particle, Set<Neighbour>> neighbours = cellIndexMethod.apply(positions, 2);
+    final Map<Particle, Set<Neighbour>> neighbours = cellIndexMethod.apply(positions, 0.9);
     for (Map.Entry<Particle, Set<Neighbour>> entry : neighbours.entrySet()) {
       System.out.println(entry.getKey());
-      for (Neighbour neighbour : entry.getValue()) {
+      for (final Neighbour neighbour : entry.getValue()) {
         System.out.println("\t" + neighbour.getNeighbourParticle());
       }
     }
 
-    MatlabFileWriter matlabFileWriter = new MatlabFileWriter();
+    final MatlabFileWriter matlabFileWriter = new MatlabFileWriter();
     matlabFileWriter.writeNeighbourParticlesFile(positions, neighbours);
-
   }
 }
