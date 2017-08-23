cc4597e7c38648492d0dc3e4bdd63bb313268b8d
fbartolome <fbartolome18@gmail.com>
2017-08-23 13:56:12 -0300

    Begin OffLaticeAutomata.

---
+++ TP1/src/main/java/ar/edu/itba/ss/automata/OffLaticeAutomata.java
@@ -0,0 +1,17 @@
+package ar.edu.itba.ss.automata;
+
+import ar.edu.itba.ss.model.Particle;
+import java.util.List;
+
+public class OffLaticeAutomata implements Runnable {
+
+    private List<Particle> particles;
+    private static double rc;
+    private static double dt;
+    
+
+    @Override
+    public void run() {
+
+    }
+}
